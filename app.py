24a96060cc8c36389c39bd9305bab32d8da3bc38
Jessica Rangel <jessicak.rangel23@gmail.com>
2026-03-08 09:21:42 +0000

    Primera version

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from datetime import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    fecha = datetime.now().strftime("%d/%m/%Y")
+    mensaje = os.environ.get('MENSAJE', '¡Hola desde Azure App Service!')
+    return f"""
+    <h1>{mensaje}</h1>
+    <p>Esta es mi primera aplicación en PaaS con Python en Azure</p>
+    <p>Fecha: {fecha}</p>
+    """
+
+if __name__ == '__main__':
+    app.run()

658b0823290cbaefe0450c828d535991ea8141d9
Jessica Rangel <jessicak.rangel23@gmail.com>
2026-03-08 03:54:16 -0600

    Update app.py
    saludos

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     fecha = datetime.now().strftime("%d/%m/%Y")
-    mensaje = os.environ.get('MENSAJE', '¡Hola desde Azure App Servicee yuju!')
+    mensaje = os.environ.get('MENSAJE', '¡Holaa jelou desde Azure App Servicee yuju!')
     return f"""
     <h1>{mensaje}</h1>
     <p>Esta es mi primera aplicación en PaaS con Python en Azure</p>
